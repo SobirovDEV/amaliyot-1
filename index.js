0f939f6bee5a980319bb7639aaa3eda93c73f6fd
sobirovDEV <rozmamasobirov991@gmail.com>
2021-10-12 12:09:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const numberOfNews = +prompt("siz qancha yangilik ko'rdiz?");
+
+const personalNewDB = {
+  const: numberOfNews,
+  news: {},
+  acters: {},
+  gennrs: [],
+  privet: false,
+};
+
+const a = prompt("oxiri ko'rgan yangliklarizdan biri"),
+  b = prompt("Unga qanha baxo bergagan bolar edingiz");
+
+personalNewDB.news[a] = b;
+
+console.log(personalNewDB);
