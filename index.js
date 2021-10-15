03c02f048ed1e1555abbbdfab45841f8c76cb0b2
sobirovDEV <rozmamasobirov991@gmail.com>
2021-10-15 21:50:21 +0300

    yangi qoshildi

--- index.js
+++ index.js
@@ -1,68 +1,61 @@
 "use strict";
 
-const a = prompt("x", "");
-console.log(a);
-// const numberOfNews = +prompt("siz qancha yangilik ko'rdiz?");
+let numberOfNews;
 
-// const personalNewDB = {
-//   const: numberOfNews,
-//   news: {},
-//   acters: {},
-//   gennrs: [],
-//   privet: false,
-// };
+function startProject() {
+  numberOfNews = +prompt("siz qancha yangilik kordiz");
 
-// const a = prompt("oxiri ko'rgan yangliklarizdan biri"),
-//   b = prompt("Unga qanha baxo bergagan bolar edingiz");
-
-// personalNewDB.news[a] = b;
-
-// console.log(personalNewDB);
-
-// let x = 5;
-//  the first method
-// while (x <= 10) {
-//   console.log(x);
-//   x++;
-// }
-// 2 usul
-// do {
-//   console.log(x);
-//   x++;
-// } while (x < 10);
-
-// 3 yoli
-
-// for (let i = 0; i < 10; i++) {
-//   if (i == 5) {
-//     // break;
-//     continue;
-//   }
-//   console.log(i);
-// }
-
-// js 14
-
-const obj = +prompt("siz qancha yangilik kordiz");
+  while (numberOfNews == "" || numberOfNews == null || isNaN(numberOfNews)) {
+    numberOfNews = +prompt("siz qancha yangilik kordiz");
+  }
+}
+startProject();
 
 const practikom = {
-  count: obj,
+  count: numberOfNews,
   news: {},
   action: {},
   genser: [],
   privat: false,
 };
-
-for (let i = 0; i < 2; i++) {
-  const a = prompt("oxirgi korgan yangilliz"),
-    b = prompt("unga qancha baho bergan bolardiz ?");
-  if (a !== null && b != "" && a != "" && b != null && a.length < 50) {
-    practikom.news[a] = b;
-    console.log("tayyor");
-  } else {
-    console.log("ERROR");
-    i--;
+function rememberNews() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("oxirgi korgan yangilliz"),
+      b = prompt("unga qancha baho bergan bolardiz ?");
+    if (a !== null && b != "" && a != "" && b != null && a.length < 50) {
+      practikom.news[a] = b;
+      console.log("tayyor");
+    } else {
+      console.log("Error");
+      i--;
+    }
   }
 }
 
-console.log(practikom);
+function showPesonalLevel() {
+  if (practikom.count < 10) {
+    console.log("juda oz sonli yangiliklar oqilibdi");
+  } else if (practikom.count < 30) {
+    console.log("siz ortacha yangiliklarni oqibsz");
+  } else if (practikom) {
+    console.log("siz alo darajad oqibsz");
+  } else {
+    console.log("xato yuz berdi");
+  }
+}
+showPesonalLevel();
+rememberNews();
+
+function showMyDb(hidden) {
+  if (!hidden) {
+    console.log(practikom);
+  }
+}
+
+showMyDb(practikom.privat);
+function writeYourGenrres() {
+  for (let i = 1; i <= 3; i++) {
+    practikom.genser[i - 1] = prompt("sizning sevimli junoringiz nomeri ${i}");
+  }
+}
+writeYourGenrres();
